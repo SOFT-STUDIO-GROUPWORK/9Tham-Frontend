a09f21b4148bc0bf9939a2b822ebfa7ff1ae5dc5
Sirawit <popeye-zz@hotmail.com>
2022-04-13 18:59:59 +0700

    add tailwind css and navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1>hello from REACT</h1>
+    <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }

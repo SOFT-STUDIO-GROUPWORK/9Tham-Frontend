0273a58b099e56b742f0f1e3e2e4b908f44d7228
gutMB <gut.pudinan@gmail.com>
2022-04-15 01:15:23 +0700

    demo ManageAccount

--- src/pages/ManageAccountPage/ManageAccountPage.tsx
+++ src/pages/ManageAccountPage/ManageAccountPage.tsx
@@ -5,15 +5,85 @@
 // (mean "ts" = "typescript" , "rafce" = "react arrow function component export default")
 // by Pop (delete if already read)
 
-import React from 'react'
+import React, { useState } from "react";
+import Selector from "./components/Selector";
+import Searchbar from "./components/Searchbar";
+import Pagination from "./components/Pagination";
 
-type Props = {}
+type Props = {};
+const Options: string[] = ["User", "Admin"];
+interface IAccount {
+  pic?: string;
+  id: string;
+  username: string;
+  name: string;
+  surname: string;
+  role: string;
+  status: string;
+}
+let mockAccount = [
+  {
+    id: "1",
+    username: "gutsoo",
+    name: "Pudinan",
+    surname: "Pensuk",
+    role: "Admin",
+    status: "Active",
+  },
+  {
+    id: "2",
+    username: "popkakza",
+    name: "Sirawit",
+    surname: "Eiei",
+    role: "Admin",
+    status: "Active",
+  },
+];
 
 const ManageAccountPage = (props: Props) => {
   // check at my homePage before proceed by pop (delete if already read)
+  const [Account, setAccount] = useState<IAccount[]>(mockAccount);
   return (
-    <div>ManageAccountPage</div>
-  )
-}
+    <div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-row m-2">
+          <div className="text-3xl">จัดการบัญชีผู้ใช้</div>
+          <Searchbar />
+          <Selector title="หมวดหมู่:" options={Options} />
+          <button className="text-base rounded-full bg-amber-500 border-8 border-transparent mx-2">
+            เพิ่มบัญชี +
+          </button>
+        </div>
+        <table className="table-auto w-5/6 border-b-2 border-gray-300">
+          <thead className="border-b-2 border-gray-300">
+            <tr className="bg-amber-500 h-12 text-left">
+              <th className="text-center">รูปภาพ</th>
+              <th>ID</th>
+              <th>บัญชีผู้ใช้</th>
+              <th>ชื่อ</th>
+              <th>สกุล</th>
+              <th>กลุ่มผู้ใช้</th>
+              <th>สถานะ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Account.map((e) => (
+              <tr className="text-sm text-left">
+                <th className="text-center">รูปภาพ</th>
+                <th>{e.id}</th>
+                <th>{e.username}</th>
+                <th>{e.name}</th>
+                <th>{e.surname}</th>
+                <th>{e.role}</th>
+                <th>{e.status}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Pagination />
+    </div>
+  );
+};
 
-export default ManageAccountPage
+export default ManageAccountPage;
---
+++ src/pages/ManageAccountPage/components/Selector.tsx
@@ -0,0 +1,28 @@
+type Props = {
+  title: string;
+  options: string[];
+};
+
+const Selector = (props: Props) => {
+  return (
+    <div className="relative flex flex-row items-center">
+      <span className="pr-2">{props.title}</span>
+      <select className="w-40 appearance-none bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 shadow-sm rounded-md leading-tight focus:outline-none focus:shadow-outline">
+        {props.options.map((opt, index) => {
+          return <option key={index}>{opt}</option>;
+        })}
+      </select>
+      <span className="absolute right-4">
+        <svg
+          className="fill-current h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </svg>
+      </span>
+    </div>
+  );
+};
+
+export default Selector;

60ad9203800c583ebe20a44842ecd3fda8253796
Sirawit <popeye-zz@hotmail.com>
2022-04-15 18:13:04 +0700

    add link to register and login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,12 +38,12 @@ const Navbar = () => {
             >
               หน้าแรก
             </a>
-            <a
-              href="#responsive-header"
+            <Link
+              to={"/login"}
               className="block mt-4 lg:inline-block lg:mt-0 text-gray-50 hover:text-white mr-4"
             >
               เข้าสู่ระบบ
-            </a>
+            </Link>
             <a
               href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-gray-50 hover:text-white"
@@ -53,7 +53,7 @@ const Navbar = () => {
           </div>
           <div>
             {/* register page */}
-            <Link to={"/"}>
+            <Link to={"/register"}>
               <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
                 ลงทะเบียน
               </button>
